26829ae80d27f85ac45a33fd43344d03e9a3cf34
bmblb3 <132216831+bmblb3@users.noreply.github.com>
2025-03-31 10:36:58 +0200

    feat: disable python3 provider

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Makes a massive difference to speedup
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_python_provider = 0
