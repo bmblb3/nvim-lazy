cdcebe01891909ccea49b50b22f5928e3579c3ef
bmblb3 <132216831+bmblb3@users.noreply.github.com>
2025-04-06 19:59:57 +0200

    feat(molten): add keymaps, pin to version 1.0.*

--- lua/plugins/molten.lua
+++ lua/plugins/molten.lua
@@ -1,5 +1,6 @@
 return {
   "benlubas/molten-nvim",
+  version = "^1.0.0",
   build = ":UpdateRemotePlugins",
   config = function()
     local map = function(mode, lhs, rhs, desc)
@@ -11,6 +12,10 @@ return {
     map("n", "<leader>re", ":MoltenEvaluateOperator<CR>", "Run operator selection")
     map("n", "<leader>rl", ":MoltenEvaluateLine<CR>", "Evaluate line")
     map("n", "<leader>rr", ":MoltenReevaluateCell<CR>", "Re-evaluate cell")
+    map("n", "<leader>rc", ":MoltenInterrupt<CR>", "Interrupt")
+    map("n", "<leader>rn", ":MoltenNext<CR>", "GoTo Next")
+    map("n", "<leader>rp", ":MoltenPrev <CR>", "GoTo Prev")
+    map("n", "<leader>rs", ":MoltenRestart! <CR>", "Reset")
 
     -- Visual mode mapping (only once)
     map("v", "<leader>r", ":<C-u>MoltenEvaluateVisual<CR>gv", "Evaluate visual selection")
