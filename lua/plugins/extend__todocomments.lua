b30ca3025f820268fbc6149ec0440e5ff88ca1eb
bmblb3 <132216831+bmblb3@users.noreply.github.com>
2025-04-22 20:17:38 +0200

    refactor(todocomments): add PARAM: todo comment

---
+++ lua/plugins/extend__todocomments.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    keywords = {
+      DOC = { icon = "󰈙 ", color = "info" },
+      PARAM = { icon = " ", color = "info" },
+    },
+  },
+}
--- lua/plugins/extend_todocomments.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = { keywords = { DOC = { icon = "󰈙 ", color = "info" } } },
-}
